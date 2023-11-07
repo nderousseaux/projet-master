226fb32ae447325d76ca3af71fba14bf5f84b6f6
Loïc DIEBOLT <loic@dblt.fr>
2023-11-07 18:39:12 +0100

    Début ajout JS graph

---
+++ site/scripts/graphique.js
@@ -0,0 +1,170 @@
+/**
+ * Paramètre le graphique, récupère les données et affiche l'ensemble
+ *
+ * @param {int} numChamp - Numéro du champ
+ * @param {int} numIlot - Numéro de l'ilot
+ * @param {string} duree - Durée d'affichage des données météo
+ * @param {string} typeMesures - Type de mesures à récupérer
+ * @returns {Array} - Tableau contenant les valeurs minimales et maximales
+ */
+function afficherGraphique(numChamp, numIlot, duree, typeMesures) {
+	let typeMesuresStr, unite, degrade;
+	if (typeMesures === "humi") {
+		typeMesuresStr = "Humidité";
+		degrade = new CouleursDonneesHum();
+		unite = " %";
+		min = 25;
+		max = 75;
+	}
+	else if (typeMesures === "temp") {
+		typeMesuresStr = "Température";
+		degrade = new CouleursDonneesTemp();
+		unite = "°C";
+		min = 10;
+		max = 30;
+	}
+	else if (typeMesures === "lumi") {
+		typeMesuresStr = "Luminosité";
+		degrade = new CouleursDonneesLumi();
+		unite = " lux";
+		min = 0;
+		max = 100000;
+	}
+	else {
+		console.error("Type de mesures inconnu \"" + typeMesures + "\"");
+		return;
+	}
+
+	// Configure les paramètres du graphique
+	const config = {
+		locale: "fr",
+		responsive: true,
+		displayModeBar: false,
+		showAxisDragHandles: false
+	}
+
+	// Récupère les couleurs en fonction du thème
+	let couleursGraph =
+		(window.matchMedia("(prefers-color-scheme: light)").matches) ?
+		new CouleursClaires() : new CouleursSombres();
+	let [bgColor, gridColor, fontColor, lineColor, fillColor] =
+		couleursGraph.getCouleursTableau();
+
+	let retourDonnees;
+
+	// Récupère les données et affiche le graphique
+	recupAbsOrdGraph(numChamp, numIlot, duree, typeMesures)
+	.then(retour => {
+		let abscisse = JSON.parse(retour[0]);
+		let ordonnee = JSON.parse(retour[1]);
+
+		// Détermine la valeur minimale de l'axe des ordonnées
+		let maxOrd = Math.max.apply(Math, ordonnee) + 5;
+		let minOrd = Math.min.apply(Math, ordonnee) - 5;
+
+		// Détermine le zoom (range) minimum du graphique
+		let rangeMin = abscisse[0];
+		let rangeMax = abscisse[abscisse.length - 1]
+		retourDonnees = Array(rangeMin, rangeMax, minOrd, maxOrd);
+
+		// Configure le placement du graphique, les ticks et les labels
+		let [top, right, bottom, left, nTicks, tickAngle, formatMois] =
+			[10, 10, 40, 55, 8, 0, "%B"];
+
+		// Adapate le graphique en fonction de la taille de l'écran
+		if (window.matchMedia("(max-width: 769px)").matches) {
+			rangeMin = abscisse[abscisse.length - (7 * 24) - 1];
+			[top, right, bottom, left, nTicks, tickAngle, formatMois] =
+				[0, 7, 70, 23, 6, -70, "%b"];
+		}
+
+		// Configure les données
+		const data = [{
+			x: abscisse,
+			y: ordonnee,
+			fill: "tozeroy",
+			fillcolor: fillColor,
+			mode: "lines+markers",
+			connectgaps: true,
+			marker: {
+				colorscale: degrade.getPairesPourcentCouleur(),
+				color: ordonnee,
+				size: 6,
+				cmin: min,
+				cmax: max
+			},
+			line: {
+				color: lineColor,
+				width: 2,
+				shape: "spline"
+			},
+			hovertemplate: "<b>" + typeMesuresStr + " :</b> %{y:.1f}" + unite +
+							"<br><b>Date :</b> %{x|%a %-d " + formatMois +
+							" à %Hh%M}<extra></extra>",
+			hoverlabel: {
+				align: "left",
+				bordercolor: "transparent",
+				font: {
+					family: "Open Sans",
+					color: "#000000"
+				}
+			}
+		}];
+
+		// Configure le style
+		const layout = {
+			showlegend: false,
+			separators: ".,",
+			plot_bgcolor: bgColor,
+			paper_bgcolor: bgColor,
+			margin: {
+				t: top,
+				r: right,
+				b: bottom,
+				l: left,
+				pad: 4
+			},
+			font: {
+				family: "Open Sans",
+				color: fontColor
+			},
+			xaxis: {
+				showgrid: false,
+				nticks: nTicks,
+				tickangle: tickAngle,
+				range: [rangeMin, rangeMax],
+				tickformatstops: [
+					{
+						"dtickrange": [null, 60000],
+						"value": "%-d " + formatMois + "<br>%Hh%M.%S"
+					},
+					{
+						"dtickrange": [60000, 3600000],
+						"value": "%-d " + formatMois + "<br>%Hh%M"
+					},
+					{
+						"dtickrange": [3600000, 86400000],
+						"value": "%-d " + formatMois + "<br>%Hh%M"
+					},
+					{
+						"dtickrange": [86400000, null],
+						"value": "%-d " + formatMois
+					}
+				]
+			},
+			yaxis: {
+				gridcolor: gridColor,
+				gridcolorwidth: 1,
+				range: [minOrd, maxOrd],
+				nticks: nTicks,
+				tickangle: tickAngle,
+				zeroline: false,
+				fixedrange: true,
+				tickformat: ".1f",
+			}
+		}
+
+		// Affiche le graphique
+		Plotly.newPlot("graph", data, layout, config);
+	})
+}
--- site/scripts/recupDonnees.js
+++ site/scripts/recupDonnees.js
@@ -33,6 +33,67 @@ function recupMoyTmpHumiLumiChamp(numChamp) {
 }
 
 /**
+ * Récupère les mesures pour un ilot, pour une durée donnée et pour un type de
+ * mesures
+ *
+ * @param {int} numChamp - Numéro du champ
+ * @param {int} numIlot - Numéro de l'ilot
+ * @param {string} duree - Durée d'affichage des données météo
+ * @param {string} typeMesures - Type de mesures à récupérer
+ * @returns {promise} les données sous format json
+ **/
+function recupMesuresIlot(numChamp, numIlot, duree, typeMesures) {
+	return new Promise((resolve, reject) => {
+		// Champ à envoyer au back, pour indiquer la colonne à récupérer
+		let champPost = new FormData();
+		champPost.append("numChamp", numChamp);
+		champPost.append("numIlot", numIlot);
+		champPost.append("duree", duree);
+		champPost.append("typeMesures", typeMesures);
+
+		// Récupère les dates des mesures et les données de la colonne demandée
+		fetch("../backend/recupMesuresIlot.php", {
+			method: "POST",
+			body: champPost
+		})
+		.then(reponse => {
+			reponse.json()
+				.then(donnees => {
+					// console.log(donnees); // [DEBUG] Pour Lucas
+					resolve(donnees);
+				})
+				.catch(err => {
+					reject(err);
+				})
+		})
+		.catch(err => {
+			reject(err);
+		})
+	})
+}
+
+/**
+ * Récupère les données de d'absisses et d'ordonnées pour le graphique
+ *
+ * @param {int} numChamp - Numéro du champ
+ * @param {int} numIlot - Numéro de l'ilot
+ * @param {string} duree - Durée d'affichage des données météo
+ * @param {string} typeMesures - Type de mesures à récupérer
+ * @returns {promise} les données sous format json
+ */
+function recupAbsOrdGraph(numChamp, numIlot, duree, typeMesures) {
+	return new Promise(resolve => {
+		recupMesuresIlot(numChamp, numIlot, duree, typeMesures)
+		.then(retour => {
+			resolve(retour);
+		})
+		.catch(err => {
+			console.log("recupAbsOrd - Erreur récupération données :", err);
+		})
+	})
+}
+
+/**
  * Récupère les données météo dans le back (pour masquer la clé API)
  *
  * @param {string} duree - Durée d'affichage des données météo

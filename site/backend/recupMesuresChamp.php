62da9c1ae900f5fd026c94d69c5b859762916743
Vincent <vincent.chebbli-disdier@etu.unistra.fr>
2023-11-19 16:05:01 +0100

    verif champs scripts php

--- site/backend/recupMesuresChamp.php
+++ site/backend/recupMesuresChamp.php
@@ -1,3 +1,30 @@
 <?php
-
+/* === recupMesuresChamp.php === */
 // Ordre 6
+const MODE_LOCAL = true;
+
+// Récupère toutes les mesures du champ envoyés dans la requête
+// Vérification de leur existence
+if (!($_POST["numChamp"])) {
+	$erreur = array("Erreur", "Champ(s) manquant(s) dans la requête");
+	echo json_encode($erreur);
+	exit();
+}
+// Vérification du type de donnée entré
+if (!(is_numeric($_POST["numChamp"]))) {
+    $erreur = array("Erreur", "Type du numéro de champ non reconnu");
+	echo json_encode($erreur);
+	exit();
+}
+// Vérification du type de donnée des boutons
+if (!(
+    $_POST["typeMesures"] === "numChamp"  // A MODIF (voir BDD)
+    )
+) {
+    $erreur = array("Erreur", "Type de mesure non reconnu");
+    echo json_encode($erreur);
+    exit();
+}
+
+// Requête à MongoDB
+// TODO

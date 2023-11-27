ed8065add1e214a1581be16abe97be57b5086d2e
Matthieu LEFEVRE <matthieu.lefevre@etu.unistra.fr>
2023-11-27 09:55:25 +0100

    Installation library MongoDB

--- infra/iaas/test_bdd.php
+++ infra/iaas/test_bdd.php
@@ -1,8 +1,11 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors',1);
+ini_set('display_errors',1); 
+require_once __DIR__ . '/vendor/autoload.php';
 try {
     $client = new MongoDB\Client("mongodb://localhost:30001");
+
+    $dbs = $client->listDatabases();
 
     echo "Connexion à MongoDB réussie !";
 

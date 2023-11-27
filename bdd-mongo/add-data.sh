bd80347e94baf77d21891cd74c2e333d02227f5d
fseel <florent.seel@etu.unistra.fr>
2023-11-27 18:03:07 +0100

    bugfix add-data.sh

--- bdd-mongo/add-data.sh
+++ bdd-mongo/add-data.sh
@@ -55,25 +55,25 @@ db.humi.insertMany( [
     {
         idAgri: 0,
         idChamps: 0,
-        dates: [new Date(), new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date(), new Date()],
         valeurs: [45.1234, 55.1234, 41.1324, 53.4845]
     },
     {
         idAgri: 0,
         idChamps: 1,
-        dates: [new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date()],
         valeurs: [47.4687, 43.1374, 55.6819]
     },
     {
         idAgri: 1,
         idChamps: 0,
-        dates: [new Date(), new Date(), new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date(), new Date(), new Date()],
         valeurs: [23, 29, 46, 44, 67]
     },
     {
         idAgri: 0,
         idChamps: 2,
-        dates: [new Date(), new Date()]
+        dates: [new Date(), new Date()],
         valeurs: [23.4674, 29.4545]
     }
 ] );
@@ -83,25 +83,25 @@ db.temp.insertMany( [
     {
         idAgri: 0,
         idChamps: 0,
-        dates: [new Date(), new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date(), new Date()],
         valeurs: [25.1234, 26.3415, 28.2684, 23.1679]
     },
     {
         idAgri: 0,
         idChamps: 1,
-        dates: [new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date()],
         valeurs: [27.4687, 23.1374, 25.6819]
     },
     {
         idAgri: 1,
         idChamps: 0,
-        dates: [new Date(), new Date(), new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date(), new Date(), new Date()],
         valeurs: [23.4568, 29.1597, 46.6479, 44.1159, 67.2345]
     },
     {
         idAgri: 0,
         idChamps: 2,
-        dates: [new Date(), new Date()]
+        dates: [new Date(), new Date()],
         valeurs: [23.9674, 24.3545]
     }
 ] );
@@ -110,25 +110,25 @@ db.lumi.insertMany( [
     {
         idAgri: 0,
         idChamps: 0,
-        dates: [new Date(), new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date(), new Date()],
         valeurs: [405, 550, 410, 337]
     },
     {
         idAgri: 0,
         idChamps: 1,
-        dates: [new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date()],
         valeurs: [945, 1000, 887]
     },
     {
         idAgri: 1,
         idChamps: 0,
-        dates: [new Date(), new Date(), new Date(), new Date(), new Date()]
+        dates: [new Date(), new Date(), new Date(), new Date(), new Date()],
         valeurs: [568, 597, 479, 159, 345]
     },
     {
         idAgri: 0,
         idChamps: 2,
-        dates: [new Date(), new Date()]
+        dates: [new Date(), new Date()],
         valeurs: [501, 808]
     }
 ] );

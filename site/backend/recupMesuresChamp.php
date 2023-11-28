f38d833a186247da1d6af5737916d13830a048d6
Vincent <vincent.chebbli-disdier@etu.unistra.fr>
2023-11-28 23:22:23 +0100

    avancée recup tableau

--- site/backend/recupMesuresChamp.php
+++ site/backend/recupMesuresChamp.php
@@ -1,18 +1,45 @@
 <?php
 
-// Vérifie que le champ est présent
-if (!(isset($_POST["numChamp"]))) {
-	$erreur = array("Erreur", "Champ manquant dans la requête");
+// Récupère les informations du champ pour le tableau
+// Vérification de leur existence
+if (!(isset($_POST["idUtilisateur"]) && isset($_POST["numChamp"]))) {
+	$erreur = array("Erreur", "Champ(s) manquant(s) dans la requête");
 	echo json_encode($erreur);
 	exit();
 }
 
-// Vérifie que le champ est numérique
+// Vérifie que les entrées sont de type numérique
 if (!(is_numeric($_POST["numChamp"]))) {
-	$erreur = array("Erreur", "Type du numéro de champ non reconnu");
+    $erreur = array("Erreur", "Type du numéro de champ non reconnu");
 	echo json_encode($erreur);
 	exit();
 }
+if (!(is_numeric($_POST["idUtilisateur"]))) {
+    $erreur = array("Erreur", "Type du numéro d'utilisateur non reconnu");
+	echo json_encode($erreur);
+	exit();
+}
+
+// Connexion à MongoDB
+use MongoDB\Driver\Manager;
+$uri = "mongodb://localhost:30001";
+
+// Créé le client
+$client = new MongoDB\Driver\Manager($uri);
+
+// Défini le filtre
+$filtre = [
+    "idAgri" => intval($_POST["idUtilisateur"]),
+    "idChamps" => intval($_POST["numChamp"]),
+];
+
+// Défini les projections
+$optionsilots = ["projection" => ["champs.ilots" => 1]];
+$options = ["projections" => ["dates" => 1]];
+
+// Créé la requête
+$requete = new MongoDB\Driver\Query($filtre, $options);
+$curdate = new DateTime("UTC");
 
 /* Requête MongoDB à faire */
 

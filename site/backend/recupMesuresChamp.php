a9c8635fbebc3d0e3903f3fa8d9649c5611ee828
Vincent <vincent.chebbli-disdier@etu.unistra.fr>
2023-12-06 10:58:57 +0100

    pb mongodb

--- site/backend/recupMesuresChamp.php
+++ site/backend/recupMesuresChamp.php
@@ -42,7 +42,6 @@ $options = ["projection" => ["dates" => 1, "valeurs" => 1]];
 $requete = new MongoDB\Driver\Query($filtre, $options);
 $curdate = new DateTime("UTC");
 
-<<<<<<< HEAD
 /* Requête MongoDB à faire */
 
 // >> Code bouchon à supprimer
@@ -56,57 +55,3 @@ else {
 	echo json_encode($erreur);
 	exit();
 }
-=======
-// Exécute la requête pour chaque type de capteur et récupère le résultat
-$resultattemp = $client->executeQuery("data.temp", $requete);
-$resultathumi = $client->executeQuery("data.humi", $requete);
-$resultatlumi = $client->executeQuery("data.lumi", $requete);
-
-// Traitement des données
-$counttemp = 0;
-$counthumi = 0;
-$countlumi = 0;
-$moytemp = 0;
-$moyhumi = 0;
-$moylumi = 0;
-$lastdtemp = null;
-$lastdhumi = null;
-$lastdlumi = null;
-
-
-$hasdeadsensor = false;
-foreach ($resultattemp as $element) {
-	if (isset($element)) {
-		foreach($element->dates as $dbdate) {
-			// Date sous format millisecondes depuis l'epoch
-			$dtdate = $dbdate->toDateTime();
-			$gap = $curdate->diff($dtdate);
-
-			// Si dernier valeur du capteur date de plus de 30 minutes, ko
-			if ($gap->format("%i") > 30) {
-				$hasdeadsensor = true;
-			}
-			else {
-				$counttemp = $counttemp + 1;
-				// On stocke la date de la derniere mesure
-				if ($lastdtemp == null || $lastdtemp < $dtdate) 
-					$lastdtemp = $dtdate;
-			}
-		}
-		foreach ($element->valeurs as $mesure) {
-			$moytemp += floatval($mesure);
-			
-		}
-	}
-	//Si pas de retour de la bdd, erreur
-	else {
-		$erreur = array("Erreur", "Retour vide de la bdd");
-		echo json_encode($erreur);
-		exit();
-	}
-}
-
-// Création des tableaux
-$res = array();
-
->>>>>>> 5165336... avancée requete mesure champ, reflexion methode

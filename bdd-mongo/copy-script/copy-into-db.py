ee1489c1c5f6ce8507419a0aa09a5c848fc3bad8
matth.lfvr <matthieu.lefevre@etu.unistra.fr>
2023-12-07 16:18:15 +0100

    retour avec un seul conteneur

--- bdd-mongo/copy-script/copy-into-db.py
+++ bdd-mongo/copy-script/copy-into-db.py
@@ -38,14 +38,12 @@ def parse_line(line):
 
 
 
-if(len(sys.argv) != 3):
-    print("Please specify the file path and IP address as cli argument.")
+if(len(sys.argv) == 1):
+    print("Please specify the file path as cli argument.")
     exit(-1)
 
-ip = sys.argv[2]
-address = "mongodb://"+ip+":30001"
 client = pymongo.MongoClient(
-        address,
+        "mongodb://mongo1:30001/",
     replicaset="rs0",
     serverSelectionTimeoutMS=5000
 )
--- bdd-mongo/copy-script/start_python.sh
+++ bdd-mongo/copy-script/start_python.sh
@@ -4,8 +4,7 @@ while true; do
 	result=$(find /data/sftp_user/upload/ -type f -mmin -2)
 	if [ -n "$result" ]; then
 		cp $result /pm/bdd-mongo/copy-script/donnees/donnees.txt
-		docker exec python python3 copy-into-db.py /data/donnees.txt 10.0.0.5
-		docker exec python python3 copy-into-db.py /data/donnees.txt 10.0.0.3
+		docker exec python python3 copy-into-db.py /data/donnees.txt
 	fi
 	sleep 120
 done

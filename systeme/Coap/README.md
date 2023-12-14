68dc21bf0412523462e3beabffc7b95180c2b6a6
vandrianandrasan <vagnona.andrianandrasana-dina@etu.unistra.fr>
2023-12-14 10:25:55 +0100

    Script Coap terminé

--- systeme/Coap/README.md
+++ systeme/Coap/README.md
@@ -6,6 +6,12 @@
 
 ```bash
 # TODO Lister les commandes à taper pour installer Coapthon3
+sudo apt install pip
+sudo pip install CoAPthon
+git clone git@github.com:Tanganelli/CoAPthon3.git
+cd CoAPthon3
+python3 setup.py sdist
+sudo pip install dist/CoAPthon3-1.0.1+fb.202312141023.tar.gz -r requirements.txt
 ```
 
 ## Serveur 
@@ -17,8 +23,8 @@ python3 oui_coapserveur.py -i 127.0.0.1 -p 5683
 ## Client 
 
 ```bash
-python3 oui_coapclient.py -o GET -p coap://127.0.0.1:5683/basic
+python3 coapclient.py -o GET -p coap://127.0.0.1:5683/basic
 
-python3 oui_coapclient.py -o POST -p coap://127.0.0.1:5683/basic -f test.txt
+python3 coapclient.py -o POST -p coap://127.0.0.1:5683/basic -f test.txt
 
 ```
--- systeme/Coap/Ressources_serveur/ressources.py
+++ systeme/Coap/Ressources_serveur/ressources.py
@@ -3,17 +3,18 @@
 from coapthon.resources.resource import Resource
 
 class Basic(Resource):
-    def __init__(self, name="basic", coap_server=None):
+    def __init__(self, file, name="basic", coap_server=None):
         super(Basic, self).__init__(name, coap_server, visible=True, observable=True, allow_children=True)
-        self.payload = "Oui studio" 
+        self.payload = "GET bien reçu"
+        self.file = file
 
     def render_GET(self, request):
         return self
 
     def render_POST(self, request):
         if request.payload is not None:
-            with open('sortie.txt', '+a') as file:
-                file.write(request.payload)
+            with open(self.file, '+a') as ffile:
+                ffile.write(request.payload)
             self.payload = request.payload 
         return self
 

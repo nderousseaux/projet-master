7d77a58d1c4745eac4805d4965081380c9282f8f
fseel <florent.seel@etu.unistra.fr>
2023-12-28 15:34:22 +0100

    adaptation script copie fichier->bdd: prend tous les fichiers dans path en arg

--- bdd-mongo/copy-script/copy-into-db.py
+++ bdd-mongo/copy-script/copy-into-db.py
@@ -1,6 +1,14 @@
+# Ce programme lit tous les fichiers dans le dossier donné en agument (relatif ou absolu)
+# et ajoute les entrees qui s'y trouvent dans la BdD Mongo.
+# Ces entrees sont de la forme (1 par ligne):
+#   date idAgri idChamps idIlot temperature humidite luminosite
+
+
 import pymongo
 from datetime import datetime
 import sys
+import os
+import glob
 
 def parse_line(line):
     parts = line.strip().split(';')
@@ -39,7 +47,7 @@ def parse_line(line):
 
 
 if(len(sys.argv) == 1):
-    print("Please specify the file path as cli argument.")
+    print("Please specify the folder path as cli argument.")
     exit(-1)
 
 client = pymongo.MongoClient(
@@ -50,13 +58,15 @@ client = pymongo.MongoClient(
 
 db = client['data']
 
-file_path = sys.argv[1]
-with open(file_path, 'r') as file:
-    for line in file:
-        temp, humi, lumi = parse_line(line)
-        db['temp'].insert_one(temp)
-        db['humi'].insert_one(humi)
-        db['lumi'].insert_one(lumi)
+folder_path = sys.argv[1]
+pattern = os.path.join(folder_path, '*')
+
+for file_path in glob.glob(pattern):
+    with open(file_path, 'r') as file:
+        for line in file:
+            temp, humi, lumi = parse_line(line)
+            db['temp'].insert_one(temp)
+            db['humi'].insert_one(humi)
+            db['lumi'].insert_one(lumi)
 
 client.close()
-

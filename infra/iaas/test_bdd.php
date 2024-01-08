fe0395446f3a7befa92262707aea7285e61836c0
matth.lfvr <matthieu.lefevre@etu.unistra.fr>
2024-01-08 18:08:29 +0100

    Dockerfile pour web avec librairie mongodb grâce à composer

--- infra/iaas/test_bdd.php
+++ infra/iaas/test_bdd.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors',1); 
+require_once __DIR__ . '/vendor/autoload.php';
 try {
     $client = new MongoDB\Client("mongodb://localhost:30001");
 

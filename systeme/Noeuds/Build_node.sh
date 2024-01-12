d924d3f197a235b1a31fa9612b49786aba403f84
CHEEKHOOREE LUCAS <lucas.cheekhooree@etu.unistra.fr>
2024-01-12 19:28:19 +0100

    correction script build node

--- systeme/Noeuds/Build_node.sh
+++ systeme/Noeuds/Build_node.sh
@@ -9,8 +9,8 @@ sudo systemctl disable NetworkManager
 
 
 #Configuration noeud
-cp start-batman-adv.sh ~/start-batman-adv.sh
-chmod +x ~/start-batman-adv.sh
+cp start-batman-adv.sh /home/$SUDO_USER/start-batman-adv.sh
+chmod +x /home/$SUDO_USER/start-batman-adv.sh
 echo 'batman-adv' | sudo tee --append /etc/modules
 echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
 

f489aa29d13164d1b694707dac8def3fcbcc5160
CHEEKHOOREE LUCAS <lucas.cheekhooree@etu.unistra.fr>
2024-01-15 17:41:09 +0100

    add firewall

--- systeme/Gateway/start-batman-adv.sh
+++ systeme/Gateway/start-batman-adv.sh
@@ -6,11 +6,33 @@ sudo ifconfig bat0 mtu 1468
 # Tell batman-adv this is an internet gateway
 sudo batctl gw_mode server
 
-# Enable port forwarding
+# Firewall
+# sudo iptables -P INPUT DROP
+# sudo iptables -P FORWARD DROP
+# sudo iptables -P OUTPUT DROP
+
+# FORWARD
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i bat0 -o eth0 -j ACCEPT
+# Bloquer la communication sortante du réseau mesh
+# A TESTER AVANT D'ENLVER LES COMMENTAIRES
+# CA FONCTIONNE MAIS POUR LE MOMENT ON NE SAIT PAS SI CA NE BLOQUE
+# PAS L'EMITION VERS L'INFRA
+#
+# sudo iptables -A FORWARD -i bat0 -o eth0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# OU
+# sudo iptables -A FORWARD -i bat0 -o eth0 -j DROP
+
+# INPUT
+# sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+# sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELAD -j ACCEPT
+
+# OUTPUT
+# sudo iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+# sudo iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+# sudo iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 
 # Mise en place de wlan0
 sudo ifconfig wlan0 down

b07da8fdc47c4cabadada8b14aa0d62ef8dc10a5
SEEL FLORENT <florent.seel@etu.unistra.fr>
2024-01-22 19:16:31 +0100

    connexion réparée pour infra

--- site/backend/deconnexion.php
+++ site/backend/deconnexion.php
@@ -6,8 +6,12 @@
 // session_save_path("/alloc");
 // session_start();
 
-session_unset();
-session_destroy();
+// session_unset();
+// session_destroy();
+if (isset($_COOKIE['idUser'])) {
+    unset($_COOKIE['idUser']); 
+    setcookie('idUser', '', -1, '/'); 
+}
 
 // redirection vers la page de connexion
 echo "<script>location.href='../connexionCmpt.php';</script>";
